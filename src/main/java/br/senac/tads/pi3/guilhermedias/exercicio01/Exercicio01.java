543a70c60c2b8e1abfe18048f47a50cd2e83cc17
guilherme.mmmdias <guilherme.mmmdias@CAS411429C14730.senacsp.edu.br>
2015-08-14 22:15:17 -0300

    main method included

--- src/main/java/br/senac/tads/pi3/guilhermedias/exercicio01/Exercicio01.java
+++ src/main/java/br/senac/tads/pi3/guilhermedias/exercicio01/Exercicio01.java
@@ -5,12 +5,26 @@
  */
 package br.senac.tads.pi3.guilhermedias.exercicio01;
 
+import java.util.Scanner;
+
 /**
  *
  * @author guilherme.mmmdias
  */
 public class Exercicio01 {
     public static void main(String[] args) {
-        System.out.println("");
+        Scanner imput=new Scanner(System.in);
+        System.out.print("Nome: ");
+        String nome=imput.next();
+        System.out.print("Nascimento: dia, mes, ano");
+        int dia=imput.nextInt();
+        int mes=imput.nextInt();
+        int ano=imput.nextInt();
+        System.out.print("Email: ");
+        String email=imput.next();
+        System.out.print("Telefone: ");
+        String tel=imput.next();
+        
+        System.out.printf("\nNome: %s\nNascimento: %d/%d/%d\nE-mail: %s\nTel: %s\n",nome,dia,mes,ano,email,tel);
     }
 }
